67c08658e017b3ac71d9099b251ed7a8eae81844
Bhavesh Damor <bhaveshdamor5555@gmail.com>
2022-09-15 22:27:01 +0530

    blog page added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import {Routes,Route} from 'react-router-dom'
+
+import Blog from './Pages/Blog.jsx'
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      <Routes>
+        <Route exact path="/" element={<Blog/>} />
+      </Routes>
+    </>
   )
 }
 
