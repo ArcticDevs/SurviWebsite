a2d7805230874d89e866ed723abda58420fe3074
Sushant <sushantgangwar87@gmail.com>
2022-09-16 18:05:23 +0530

    Updated Files

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import { AiOutlineDown } from 'react-icons/ai'
 import useBreakpoints from '../customHooks/useBreakpoints'
 
@@ -10,37 +11,37 @@ const Navbar = () => {
         <header>
             <nav className="navbar fixed-top navbar-expand-xl">
                 <div className="container-fluid mx-auto nav_cont">
-                    <a className="navbar-brand" href="/">Navbar</a>
+                    <Link className="navbar-brand" to="/">Navbar</Link>
                     <button className="navbar-toggler border-0" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav mb-2 mb-lg-0 nav_list" style={{ marginLeft: 'auto' }}>
                             <li className="nav-item dropdown">
-                                <a className="nav-link dropdown_btn active" href="/" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                <Link className="nav-link dropdown_btn active" to="/" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                     Home
                                     <AiOutlineDown className="arrow_icon" />
-                                </a>
+                                </Link>
                                 <ul className="dropdown-menu rounded-0 border-0" aria-labelledby="navbarDropdown">
-                                    <li><a className="dropdown-item active" href="/">Home v1.1</a></li>
-                                    <li><a className="dropdown-item" href="/">Home v1.2</a></li>
-                                    <li><a className="dropdown-item" href="/">Home v1.3</a></li>
+                                    <li><Link className="dropdown-item active" to="/">Home v1.1</Link></li>
+                                    <li><Link className="dropdown-item" to="/">Home v1.2</Link></li>
+                                    <li><Link className="dropdown-item" to="/">Home v1.3</Link></li>
                                 </ul>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="/">About</a>
+                                <Link className="nav-link" to="/">About</Link>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="/">Services</a>
+                                <Link className="nav-link" to="/">Services</Link>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="/">Gallery</a>
+                                <Link className="nav-link" to="/">Gallery</Link>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="/blog">Blog</a>
+                                <Link className="nav-link" to="/blog">Blog</Link>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="/">Contact</a>
+                                <Link className="nav-link" to="/">Contact</Link>
                             </li>
                             {!isXl &&
                                 <li className="ms-5">
